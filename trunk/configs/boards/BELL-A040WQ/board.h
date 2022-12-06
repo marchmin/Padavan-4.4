5d63b5c0861e77abe0023b154d226e5ff7a22d55
hy5528 <95360525+hy5528@users.noreply.github.com>
2022-12-07 07:40:12 +0800

    Update board.h

--- trunk/configs/boards/BELL-A040WQ/board.h
+++ trunk/configs/boards/BELL-A040WQ/board.h
@@ -1,7 +1,6 @@
-/* BELL-A-040W-Q */
-
-#define BOARD_PID		"BELL-A-040W-Q"
-#define BOARD_NAME		"BELL-A-040W-Q"
+/* BELL-A040WQ */
+#define BOARD_PID		"BELL-A040WQ"
+#define BOARD_NAME		"BELL-A040WQ"
 #define BOARD_DESC		"nokia-sbell A-040W-Q Wireless Router"
 #define BOARD_VENDOR_NAME	"nokia-sbell"
 #define BOARD_VENDOR_URL	"http://www.nokia-sbell.com/"
@@ -23,6 +22,7 @@
 #undef BOARD_GPIO_LED_ROUTER
 #undef BOARD_GPIO_PWR_USB_ON
 #undef BOARD_GPIO_PWR_USB
+
 #define BOARD_HAS_5G_11AC	1
 #define BOARD_NUM_ANT_5G_TX	2
 #define BOARD_NUM_ANT_5G_RX	2
@@ -31,5 +31,5 @@
 #define BOARD_NUM_ETH_LEDS	1
 #define BOARD_HAS_EPHY_L1000	1
 #define BOARD_HAS_EPHY_W1000	1
-#undef BOARD_NUM_UPHY_USB3
+#undef BOARD_NUM_UPHY_USB3      0
 #define BOARD_USB_PORT_SWAP	1
